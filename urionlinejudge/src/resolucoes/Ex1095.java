93e720916ecbe5aa6ec7e6b07896011d4df76f28
Renato Pereira Santos <renatodev95@gmail.com>
2021-08-18 08:05:19 -0300

    Create Ex1095.java

---
+++ urionlinejudge/src/resolucoes/Ex1095.java
@@ -0,0 +1,17 @@
+package resolucoes;
+
+public class Ex1095 {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		int j = 60;
+		
+		while (j >= 0) {
+			System.out.printf("I=%d J=%d\n", i, j);
+			
+			i += 3;
+			j -= 5;
+		}
+	}
+}
